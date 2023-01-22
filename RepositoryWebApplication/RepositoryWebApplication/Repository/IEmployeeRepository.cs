441943fdedc0827fe7b621adf6a0a2fd486c3627
codilarkrishna <krishna.kk0106@gmail.com>
2023-01-23 00:33:37 +0530

    C1: committing Repository Design pattern

---
+++ RepositoryWebApplication/RepositoryWebApplication/Repository/IEmployeeRepository.cs
@@ -0,0 +1,18 @@
+﻿using RepositoryWebApplication.DAL;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace RepositoryWebApplication.Repository
+{
+    public interface IEmployeeRepository
+    {
+        IEnumerable<Employee> GetAll();
+        Employee GetById(int EmployeeID);
+        void Insert(Employee employee);
+        void Update(Employee employee);
+        void Delete(int EmployeeID);
+        void Save();
+    }
+}
